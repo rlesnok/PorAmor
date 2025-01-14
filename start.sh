f9f3414790c68cd5462ce154301f5f869274af73
Raphael <rlesnok@gmail.com>
2025-01-14 08:15:25 -0300

    Adiciona configuração para instalar driver ODBC no Render

--- start.sh
+++ start.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
+
+# Instalar o ODBC Driver 17 for SQL Server
+curl https://packages.microsoft.com/keys/microsoft.asc | apt-key add -
+curl https://packages.microsoft.com/config/ubuntu/20.04/prod.list > /etc/apt/sources.list.d/mssql-release.list
+apt-get update
+ACCEPT_EULA=Y apt-get install -y msodbcsql17 unixodbc-dev
+
+# Rodar o aplicativo Flask
 gunicorn app:app --bind 0.0.0.0:$PORT
