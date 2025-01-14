1092567cfb7b6bb35413123f895143023df9be6d
Raphael <rlesnok@gmail.com>
2025-01-14 08:19:11 -0300

    Corrigir instação do driver ODBC no Render

--- start.sh
+++ start.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-# Instalar o ODBC Driver 17 for SQL Server
+# Adiciona o repositório da Microsoft para o driver ODBC
 curl https://packages.microsoft.com/keys/microsoft.asc | apt-key add -
 curl https://packages.microsoft.com/config/ubuntu/20.04/prod.list > /etc/apt/sources.list.d/mssql-release.list
-apt-get update
-ACCEPT_EULA=Y apt-get install -y msodbcsql17 unixodbc-dev
 
-# Rodar o aplicativo Flask
+# Atualiza os pacotes e instala o driver ODBC necessário
+apt-get update
+ACCEPT_EULA=Y apt-get install -y msodbcsql17 unixodbc
+
+# Inicia o aplicativo Flask com Gunicorn
 gunicorn app:app --bind 0.0.0.0:$PORT
